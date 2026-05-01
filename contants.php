27f068060c72b6670bbd098a265b6623dd606592
madi <mz2284326@gmail.com>
2026-05-01 12:44:39 +0330

    Create contants.php

---
+++ contants.php
@@ -0,0 +1,9 @@
+<?php
+ 
+define(&quotHOST&quot , &quotlocalhost&quot);
+define(&quotUSERNAME&quot , &quotroot&quot);
+define(&quotPASSWORD&quot , &quot&quot);
+define(&quotDB&quot , &quotlink_db&quot);
+define(&quotCURRENT_TIME&quot , date(&quotY/m/d H:i:s&quot));
+ 
+?>
